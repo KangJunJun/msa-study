ccabe896e5e89bfeb592feb2d3773bfe92ebfac1
jhkang <serdozz@naver.com>
2023-04-23 22:18:14 +0900

    init

---
+++ src/main/java/com/study/mas/MasApplication.java
@@ -0,0 +1,13 @@
+package com.study.mas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/mas/MasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
